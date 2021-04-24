b40e7ae95fcc775983fc54ccf04b84ae710d3681
ojay <jonathanonderi2018@yahoo.com>
2021-04-24 15:07:13 +0300

    update

--- src/components/inputBox/index.jsx
+++ src/components/inputBox/index.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './Inputbox.module.css'
 
 export const InputBox = (props) => {
+    const isPassword = props.type === "password"
+    const inputRef = useRef(null)
+    const togglePassword = (e) => {
+        e.preventDefault()
+        if (inputRef.current.type === "password"){
+            inputRef.current.type ="text"
+        }
+        else{
+            inputRef.current.type ="password"
+        }
+    }
+    
     return <div>
         <div className="input-field ">
             <input
                 className={styles.input_text}
                 {...props}
+                ref={inputRef}
 
             />
+            {isPassword && <a href="#" onClick={togglePassword}>
+                <span class="material-icons">
+                {inputRef.current.type ==="password"? "visibility":"visibility_off" }
+            </span></a>}
         </div>
     </div>
+}
+const VisilityToggle = (ele)=>{
+    return 
 }
