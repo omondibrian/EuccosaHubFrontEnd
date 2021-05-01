31cdff34e3800776bc19ee5a5aef84bc4ac0d4ec
brian <omondibrian392@gmail.com>
2021-05-01 17:42:37 +0300

    new Updates

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -54,6 +54,22 @@ export const FetchUser = async (id) => {
   }
 };
 
+export const FetchUsers = async () => {
+  const result = await fetch("http://192.168.43.154:3001/auth/users" , {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+  });
+  if (result.ok) {
+    const data = await result.json();
+    return data;
+  } else {
+    //TODO:add display incase of server error or any other errors
+    console.log(result.statusText);
+  }
+};
+
 export const registerNewUser = async (user) => {
   const formData = new FormData();
   for (const field in user) {
@@ -76,3 +92,24 @@ export const registerNewUser = async (user) => {
   };
   return result;
 };
+
+
+ export const updateProfile = async (updates) =>{
+  const result = await fetch("http://192.168.43.154:3001/auth/profile", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(updates),
+  }).catch((e) => {
+    return e;
+  });
+  const data = await result.json();
+  if (result.ok) {
+    return data;
+  }else{
+    return {
+      message:'Error updating profile please retry'
+    }
+  }
+}
